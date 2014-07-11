ec2d37ec360b9c23cf719f178d22d4df02c5a2f0
Tasmer <tasmer.b@hotmail.com>
2014-07-11 17:10:48 +0200

    Rename plugin

--- current-date-post-attachment.php
+++ current-date-post-attachment.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Post Attachment
+Plugin Name: Current date Post Attachment
 version: 1.0
 Description: Use Post Attachment for upload your media in directory Year/Month/ for the current year and month, when the date of publication of your article is different.
 Author: Beapi
@@ -10,11 +10,11 @@ add_action( 'plugins_loaded', 'init_ap_plugin' );
 
 function init_ap_plugin() {
 	if( is_admin() ) {
-		new Attachment_Post();
+		new Current_Date_Post_Attachment();
 	}
 }
 
-class Attachment_Post {
+class Current_Date_Post_Attachment {
 
 	function __construct() {
 		add_filter( 'upload_dir', array( __CLASS__, 'upload_dir' ) );
