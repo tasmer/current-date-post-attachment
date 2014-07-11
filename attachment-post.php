d28c5c97000d056dbade5aa20eb89a4dbb820138
Tasmer <tasmer.b@hotmail.com>
2014-07-11 10:39:08 +0200

    fist commit

---
+++ attachment-post.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Post Attachment
+version: 1.0
+Description: Use Post Attachment for upload your media in directory Year/Month/ for the current year and month, when the date of publication of your article is different.
+Author: Beapi
+*/
+
+register_activation_hook( __FILE__, array( 'AP_Plugin', 'activate' ) );
+add_action( 'plugins_loaded', 'init_ap_plugin' );
+
+function init_ap_plugin(){
+	if( is_admin() ){
+		new Attachment_Post();
+	}
+}
+
+class Attachment_Post {
+
+	function __construct() {
+		add_filter( 'upload_dir', array( __CLASS__, 'upload_dir' ) );
+	}
+
+	public static function upload_dir( $params ) {
+
+		if( isset( $_GET['post'] ) ) {
+			$params['subdir'] = '/' .date('Y') . '/' . date('m');
+			$params['path'] = $params['basedir'] . $params['subdir'];
+			$params['url']  = $params['baseurl'] . $params['subdir'];
+		}
+
+		return $params;
+	}
+}
