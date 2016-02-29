358376891f21f34d8f48e2b207512e9e1648d478
zainoudine <tasmer.b@gmail.com>
2016-02-29 22:28:39 +0100

    add licence edit plugin informations

--- current-date-post-attachment.php
+++ current-date-post-attachment.php
@@ -1,9 +1,28 @@
 <?php
 /*
 Plugin Name: Current date Post Attachment
-version: 1.0
 Description: Use Post Attachment for upload your media in directory Year/Month/ for the current year and month, when the date of publication of your article is different.
-Author: Beapi
+Author: Be API
+Author URI: http://beapi.fr
+version: 1.0
+
+----
+ Copyright 2016 BE API Technical team (human@beapi.fr)
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 */
 
 add_action( 'plugins_loaded', 'init_ap_plugin' );
