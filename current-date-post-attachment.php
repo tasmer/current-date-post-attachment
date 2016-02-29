086382694c85f31f428a261d630220e12c3a782e
zainoudine <tasmer.b@gmail.com>
2016-02-29 20:33:14 +0100

    add PHPdocs

--- current-date-post-attachment.php
+++ current-date-post-attachment.php
@@ -8,18 +8,32 @@ Author: Beapi
 
 add_action( 'plugins_loaded', 'init_ap_plugin' );
 
+/**
+ * Init plugin.
+ *
+ */
 function init_ap_plugin() {
 	if( is_admin() ) {
 		new Current_Date_Post_Attachment();
 	}
 }
 
+/**
+ * Class Current_Date_Post_Attachment
+ */
 class Current_Date_Post_Attachment {
 
 	function __construct() {
 		add_filter( 'upload_dir', array( __CLASS__, 'upload_dir' ) );
 	}
 
+	/**
+	 * Filter the upload directory datas
+	 *
+	 * @param array $params Array of upload directory data.
+	 *
+	 * @return mixed
+	 */
 	public static function upload_dir( $params ) {
 		if( isset( $_POST['action'] ) && $_POST['action'] == 'upload-attachment' ) {
 			$params['subdir'] = '/' .date('Y') . '/' . date('m');
